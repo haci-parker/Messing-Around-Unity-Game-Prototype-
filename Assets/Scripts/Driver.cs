820e550259993406a9f9e235a9f7cf35a5d852e4
berkay er <xxxberk4y71@gmail.com>
2026-04-28 17:28:36 +0300

    İlk commit

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float steerSpeed = 0.5f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = 0f;
+        float steer = 0f;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f; 
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Translate(0,moveAmount,0);
+        transform.Rotate(0, 0, steerAmount);
+    }
+}
